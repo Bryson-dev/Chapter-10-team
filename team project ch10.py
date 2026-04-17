3ac6dabb7777ef305116414c44741c37eae54389
Retr0Tristan <tristao.tilman@gmail.com>
2026-04-17 11:39:15 -0500

    Create team project ch10.py

---
+++ team project ch10.py
@@ -0,0 +1,28 @@
+import pickle
+
+def main():
+    #This list will hold our objects
+    inventory_list = []
+    
+    print("--- Retail Store ---")
+    print()
+    print("Please choose from the options below:")
+    print()
+    print("To access the inventory control system, press 1.")
+    print("To access the retail store, press 2.")
+    print()
+    keep_going = 'y'
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        print()
+        print("Welcome to the ACME inventory control system.")
+        print()
+        print("Inventory file doesn't exist, creating...")
+        password = input("Enter the inventory control password: ")
+        
+        
+    elif choice == 2:
+        print("Opening Retail store...")
+        
+    else:
+        print("Invalid choice.")
